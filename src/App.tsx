6985ff6f5c14b9fa57c782aaa50f63e329d18321
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-04 15:52:37 -0400

    added dogs

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,21 @@ import './App.css';
 
 
 function App() {
+
+  const [serverData, setServerData] = React.useState([])
+  React.useEffect(() => {
+    fetch('http://127.0.0.1:4000')
+    .then(res => res.json())
+    .then(data => setServerData(data))
+    .catch(err => console.log(err))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <Hello firstName='Camila' lastName='Sandoval'/>
         <Form />
+        {/* @ts-ignore */}
+        {serverData && serverData.map(eachDog => <p>{eachDog.name} - {eachDog.age}</p>)}
       </header>
     </div>
   );
